4e4561adac9a8cd997a23d5f814fe78047bf6754
Steffen Strunge Mathiesen <st.st.ma@hotmail.com>
2017-05-01 19:28:20 +0200

    Added navbars template

--- index/index.py
+++ index/index.py
@@ -2,4 +2,4 @@ from django.views.generic import TemplateView
 
 
 def index(request):
-    return TemplateView.as_view(template_name="index.html")
+    return TemplateView.as_view(template_name="navbars.html")
