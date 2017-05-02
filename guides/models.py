8b7c48ed6d7186d2ee4f48d043ee8ccafef98304
Steffen Strunge Mathiesen <st.st.ma@hotmail.com>
2017-05-02 09:57:02 +0200

    Guides page now uses data from database

--- guides/models.py
+++ guides/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+
+class Guide(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    category = models.ForeignKey(Category)
+    def __str__(self):
+        return self.title
--- guides/views.py
+++ guides/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Guide, Category
 from django.template.response import SimpleTemplateResponse
 from django.views.generic import TemplateView
 
 
 def index(request):
-    return SimpleTemplateResponse(template="guides/index.html")
+    all_guides = Guide.objects.all()
+    all_categories = Category.objects.all()
+    return render(request, 'guides/index.html', {'all_guides': all_guides, 'all_categories': all_categories})
