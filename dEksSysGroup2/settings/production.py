0d5511172040132f40c4cecb641c0d6c64eba6cd
Steffen Strunge Mathiesen <st.st.ma@hotmail.com>
2017-05-03 17:39:01 +0200

    Removed staticfiles usage

--- dEksSysGroup2/settings/production.py
+++ dEksSysGroup2/settings/production.py
@@ -125,6 +125,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 STATICFILES_DIRS = (os.path.join("static"), )
