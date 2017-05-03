972cd23e3e40b5b5cd35ea6622dadb75beb10f88
Steffen Strunge Mathiesen <st.st.ma@hotmail.com>
2017-05-03 17:45:22 +0200

    Added whitenoise usage

--- dEksSysGroup2/settings/production.py
+++ dEksSysGroup2/settings/production.py
@@ -126,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join("static"), )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
