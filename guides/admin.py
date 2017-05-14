454ba0c808016c3821530bbb2b384382120cdaa9
ArneKr <arnekratz@hotmail.de>
2017-05-14 14:22:31 -0700

    Added some more options for guide suggestions and added a suggestion button for the guide page

--- guides/admin.py
+++ guides/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Guide, Category
+from .models import Guide, Category, GuideSuggestion
 # Register your models here.
 
 admin.site.register(Guide)
 admin.site.register(Category)
+admin.site.register(GuideSuggestion)
--- guides/views.py
+++ guides/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Guide, Category
+from .models import Guide, Category, GuideSuggestion
 from django.template.response import SimpleTemplateResponse
 
 
@@ -9,9 +9,15 @@ def index(request):
     return render(request, 'guides/index.html', {'all_guides': all_guides, 'all_categories': all_categories})
 
 
-def guide(request):
-    return SimpleTemplateResponse('guides/guide.html')
+def guide(request, guideID):
+
+    return render(request, 'guides/guide.html', {'guide' : Guide.objects.get(pk=guideID)})
 
 
 def create_guide(request):
     return SimpleTemplateResponse('guides/create_guide.html')
+
+def guide_suggestions(request):
+    all_suggestions = GuideSuggestion.objects.all()
+    all_categories = Category.objects.all()
+    return render(request, 'guides/guide_suggestions.html', {'all_suggestions': all_suggestions, 'all_categories': all_categories})
