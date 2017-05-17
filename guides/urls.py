ec1737ff5bbc63d8dbd5274387c4547a9e9d26bf
Steffen Strunge Mathiesen <st.st.ma@hotmail.com>
2017-05-17 19:01:24 +0200

    Updated hardcoded links to dynamic.

--- guides/urls.py
+++ guides/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="guides"),
-    url(r'^guide/(?P<guideID>\d+)/$', views.guide, name="guide"),
+    url(r'^guide/(?P<guide_id>\d+)/$', views.guide, name="guide"),
     url(r'^create_guide', views.CreateGuide.as_view(), name="create_guide"),
     url(r'^guide_suggestions', views.guide_suggestions, name="guide_suggestions"),
     url(r'^request_guide', views.CreateRequest.as_view(), name="request_guide")
--- guides/views.py
+++ guides/views.py
@@ -11,9 +11,9 @@ def index(request):
     return render(request, 'guides/index.html', {'all_guides': all_guides, 'all_categories': all_categories})
 
 
-def guide(request, guideID):
+def guide(request, guide_id):
+    return render(request, 'guides/guide.html', {'guide': Guide.objects.get(pk=guide_id)})
 
-    return render(request, 'guides/guide.html', {'guide' : Guide.objects.get(pk=guideID)})
 
 class CreateGuide(View):
     def get(self, request):
@@ -26,14 +26,19 @@ class CreateGuide(View):
     def post(self, request):
         form = SubmitNewGuide(request.POST)
         if form.is_valid():
-            new_guide = Guide.objects.create(title = form.cleaned_data['title'], short_description = form.cleaned_data['short_description'], text = form.cleaned_data['text'],
-             category = form.cleaned_data['category'], author = request.user)
+            Guide.objects.create(title=form.cleaned_data['title'],
+                                 short_description=form.cleaned_data['short_description'],
+                                 text=form.cleaned_data['text'],
+                                 category=form.cleaned_data['category'], author=request.user)
         return index(request)
+
 
 def guide_suggestions(request):
     all_suggestions = GuideSuggestion.objects.all()
     all_categories = Category.objects.all()
-    return render(request, 'guides/guide_suggestions.html', {'all_suggestions': all_suggestions, 'all_categories': all_categories})
+    return render(request, 'guides/guide_suggestions.html',
+                  {'all_suggestions': all_suggestions, 'all_categories': all_categories})
+
 
 class CreateRequest(View):
     def get(self, request):
@@ -46,6 +51,7 @@ class CreateRequest(View):
     def post(self, request):
         form = SubmitNewRequest(request.POST)
         if form.is_valid():
-            new_request = GuideSuggestion.objects.create(name = form.cleaned_data['name'], description = form.cleaned_data['description'], category = form.cleaned_data['category'])
+            GuideSuggestion.objects.create(name=form.cleaned_data['name'],
+                                           description=form.cleaned_data['description'],
+                                           category=form.cleaned_data['category'])
         return index(request)
-
