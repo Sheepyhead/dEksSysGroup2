507271dde9a3fdcae74078c9887923ca2024f8bf
Steffen Strunge Mathiesen <st.st.ma@hotmail.com>
2017-05-18 11:28:26 +0200

    Fixed layout and style

--- guides/models.py
+++ guides/models.py
@@ -7,21 +7,26 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
 
+
 class Subcategory(models.Model):
     name = models.CharField(max_length=50)
+    super_category = models.ForeignKey(Category, null=True)
+
     def __str__(self):
         return self.name
+
 
 class GuideSuggestion(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     category = models.ForeignKey(Category)
+
     def __str__(self):
         return self.name
-
 
 
 class Guide(models.Model):
@@ -29,7 +34,7 @@ class Guide(models.Model):
     short_description = models.CharField(max_length=100, default="")
     text = models.TextField()
     category = models.ForeignKey(Category)
-    subcategory = models.CharField(max_length=20, default="General")
+    subcategory = models.ForeignKey(Subcategory)
     author = models.CharField(max_length=20, default="Anonymous")
 
     def __str__(self):
